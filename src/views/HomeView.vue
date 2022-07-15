82f8a256419d43ae8210e8f3ba4f35f2415c0a79
林凯丰 <1656399881@qq.com>
2022-07-15 18:45:45 +0800

    feat(add): 配置基准地址

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>home</div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  created() {},
+  mounted() {}
+}
+</script>
+<style lang="scss" scoped></style>
